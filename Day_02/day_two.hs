d5ee9b3fb0f92c997a07bf01369e5f9ce9628d44
Aravind Kumar <aravind@byteally.com>
2022-05-26 12:37:48 +0530

    Day 2 changes

--- Day_02/day_two.hs
+++ Day_02/day_two.hs
@@ -3,7 +3,7 @@
 -- common operators are  + , - , *, / ,div, mod, quot, rem
 -- 1+1, 1-1, 1*1, 1/1, div 1 1[Integral division], mod 1 1, quot 1 1, rem 1 1
 
---  Writing -->-100 is fine , where it throws an error for the case like,
+-- Writing -->-100 is fine , where it throws an error for the case like,
 -- Prelude> 1000 + -9000 for this case will be wrong to correct way is,
 -- Prelude> 1000 + (-9000) or 1000 + (negate 100) or 1000 + 1000 for this case will be wrong to correct way is,
 
