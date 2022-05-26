047df6526e0993803f4ad4548853721c4a7aefa1
Aravind Kumar <aravind@byteally.com>
2022-05-26 13:30:11 +0530

    Exercise added

--- Day_02/day_two.hs
+++ Day_02/day_two.hs
@@ -1,3 +1,5 @@
+import System.Win32 (COORD (x))
+
 -- Arithmetic function in Haskell
 
 -- common operators are  + , - , *, / ,div, mod, quot, rem
@@ -17,7 +19,28 @@ printInc n = print plusTwo
   where
     plusTwo = n + 2
 
-printInc2 :: (Show a, Num a) => a -> IO ()
-printInc2 n =
-  let plusTwo = n + 2
-   in print plusTwo
+-- Desurgaring let to lambda
+
+printInc2' :: (Show a, Num a) => a -> IO ()
+printInc2' n =
+  (\plusTwo -> print plusTwo) (n + 2)
+
+-- Exercise
+-- 1
+checkIs5 :: (Eq a, Num a) => a -> IO ()
+checkIs5 x = do
+  if x == 5
+    then putStrLn "Its is 5"
+    else putStrLn "Its not 5"
+
+-- 2
+multipleNum :: Num a => a -> a
+multipleNum x = x * x
+
+-- 3
+multipleDiffNum :: Num a => a -> a -> a
+multipleDiffNum x y = x * y
+
+-- 4
+multipleWithStaticNum :: Num a => a -> p -> a
+multipleWithStaticNum x y = x + 3
---
+++ Day_02/functionWithLet.hs
@@ -0,0 +1,4 @@
+printInc2 :: (Show a, Num a) => a -> IO ()
+printInc2 n =
+  let plusTwo = n + 2
+   in print plusTwo
