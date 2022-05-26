7cbf85ac1ba420892b47e20d8de2b76b321c483d
Aravind Kumar <aravind@byteally.com>
2022-05-26 12:36:57 +0530

    Day 2 works/learning

---
+++ Day_02/day_two.hs
@@ -0,0 +1,14 @@
+-- Arithmetic function in Haskell
+
+-- common operators are  + , - , *, / ,div, mod, quot, rem
+-- 1+1, 1-1, 1*1, 1/1, div 1 1[Integral division], mod 1 1, quot 1 1, rem 1 1
+
+--  Writing -->-100 is fine , where it throws an error for the case like,
+-- Prelude> 1000 + -9000 for this case will be wrong to correct way is,
+-- Prelude> 1000 + (-9000) or 1000 + (negate 100) or 1000 + 1000 for this case will be wrong to correct way is,
+
+-- Parenthesizing infix functions
+
+
+
+
