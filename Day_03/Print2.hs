2fee876a2486df90d00ad281afce889b1307e9a5
Aravind Kumar <aravind@byteally.com>
2022-05-27 11:56:48 +0530

    Small changes

--- Day_03/Print2.hs
+++ Day_03/Print2.hs
@@ -2,6 +2,15 @@ module Day_03.Print2 where
 
 main :: IO ()
 main = do
+  putStrLn "Count to four for me:)"
+  putStr "one, two"
+  putStr ", three, and"
+  putStrLn " four !"
+
+-- example if we chnage it to putStrLn to putStr and voice-versa
+
+mainRevers :: IO ()
+mainRevers = do
   putStr "Count to four for me:)"
   putStrLn "one, two"
   putStrLn ", three, and"
